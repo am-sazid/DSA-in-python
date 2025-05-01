be51e22e25587071d2f588df549e32cb1aaa14f0
am-sazid <amsazid.cse@gmail.com>
2025-05-01 12:05:39 +0600

    DSA-class1

---
+++ Extraction_of_Digits.py
@@ -0,0 +1,10 @@
+
+n = 5873
+
+num = n
+
+while num > 0:
+    digit = num % 10
+    print(digit)
+    num = num // 10
+
---
+++ count_the_num_of_digits.py
@@ -0,0 +1,33 @@
+from math import*
+
+def count_digts(num):
+    return int(log10(num)+1)
+
+n = count_digts(323535)
+
+print(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def count_digits(n):
+
+#     count = 0
+#     while n > 0:
+#         n = n // 10
+#         count += 1
+#     return count
+
+# n = count_digits(2343225)
+# print(n)
+
+
