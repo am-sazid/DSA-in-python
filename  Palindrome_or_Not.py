8c6627374e2573f73dda6bf8f3c62177fec09e71
am-sazid <amsazid.cse@gmail.com>
2025-05-04 15:03:58 +0600

    practice

---  Palindrome_or_Not.py
+++  Palindrome_or_Not.py
@@ -11,22 +11,37 @@
 # print(result)
 
 
-# n =1221
-# num= n
-# result=0
-# while num>0:
-#     ld = num%10
-#     result = (result * 10)+ld
-#     num=num//10
+n = 1221
+num= n
+result=0
+while num>0:
+    ld = num%10
+    result = (result * 10)+ld
+    num=num//10
 
-# if result == n:
+if result == n:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+# name = "sazid"
+# n = name
+# n = n[::-1]
+# print(n)
+# if n == name:
 #     print("Palindrome")
 # else:
 #     print("Not Palindrome")
 
-
-n = "sazid" 
-n = n[::-1]
-print(n)
-
 # print(1234 * 10)
+
+# S = input()
+
+# st = S
+
+# st = st[::-1]
+
+# if st == S:
+# 	print("Palindrome")
+# else:           
+#     print("Not Palindrome")
