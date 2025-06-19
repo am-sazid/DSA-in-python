10c00045a8e621a93f15b19868268096e4d6ba6c
am-sazid <amsazid.cse@gmail.com>
2025-06-19 22:41:59 +0600

    Hashing

--- Hashing.py
+++ Hashing.py
@@ -9,3 +9,17 @@ for num in m:
             count += 1
 
 print(count)
+
+
+n = [10,2,4,5,6,7,5,9,5,1,5,3]
+m = [111,2,5,4,1,3,10]
+
+hashlist = [0] * 11
+
+for num in n:
+    hashlist[num] += 1
+for num in m:
+    if num < 1 or num > 10:
+        print(0)
+    else:
+        print(hashlist[num])
