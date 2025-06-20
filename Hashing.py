3b0f0a13833a78f3efa3449ea1f97cc777b5025c
am-sazid <amsazid.cse@gmail.com>
2025-06-20 22:44:41 +0600

    Hashing

--- Hashing.py
+++ Hashing.py
@@ -23,3 +23,26 @@ for num in m:
         print(0)
     else:
         print(hashlist[num])
+
+n = [10,2,4,5,6,7,5,9,5,1,5,3]
+m = [111,2,5,4,1,3,10]
+hashlist = [0] * 11
+for num in n:
+    hashlist[num] += 1
+for num in m:
+    if num < 1 or num > 10:
+        print(0)
+    else:
+        print(hashlist[num])
+# Using a dictionary for hashing
+hash_dict = {}
+for num in n:
+    if num in hash_dict:
+        hash_dict[num] += 1
+    else:
+        hash_dict[num] = 1
+for num in m:
+    if num in hash_dict:
+        print(hash_dict[num])
+    else:
+        print(0)
